50a9b9f2482f3182e76a1dc59c1a3a5a8a06bd36
Jet <jet_pham@student.davincischools.rg>
2020-10-23 17:12:28 -0700

    Changes

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            ServerSocket server = new ServerSocket(50000);
+            Socket socket = server.accept();
+
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.writeUTF("Hello, world!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
