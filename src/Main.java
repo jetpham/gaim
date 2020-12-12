2686b0a019b15607395fc07474a6c3e4f3ef664f
Jet Pham <jetthomaspham@gmail.com>
2020-12-11 16:35:58 -0800

    ridding of unused imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Scanner;
-import Moves.*;
 
 
 public class Main {
@@ -26,17 +21,5 @@ public class Main {
         }
 
 
-
-        /*try {
-
-            ServerSocket server = new ServerSocket(50000);
-            Socket socket = server.accept();
-
-            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-            outputStream.writeUTF("Hello, world!");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
     }
 }
